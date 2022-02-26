9ecf8c439c75ed181a26c351048b6825e086ac95
Henri J. Suurorg <hsuurorg@henris-air.lan>
2022-02-26 15:53:20 +0100

    fix flickering for button 1 | rename files | clean func.c

--- work.c
+++ work.c
@@ -7,9 +7,11 @@ int count = 0;
 char textstring[] = "text, more text, and even more text!";
 
 int getbtns( void ){
-    /* shift the bits right by 5 to get the first button 
-     at the lsb, then mask the 3 switch bits and return */
-    return (PORTD >> 5) & 0x7;
+  int r;
+  r = (PORTF >> 1) & 0x1; // check button 1
+  r = r | ((PORTD >> 4) & 0xE); // check button 2-4
+
+  return r&0xF;
 }
 
 /* Interrupt Service Routine */
@@ -21,20 +23,26 @@ void user_isr( void )
     if(count == 50){
       clearDisplay();
       display_image(0, display);
-
       count = 0;
     }
 
     int btns = getbtns();
 
     if (btns){
+
+      if(btns & 0x8){
+        clearDisplay();
+        setDisplay2d(0, 16, 128, 16);
+        display_image(0, display);
+      }
+
       if(btns & 0x4){
         clearDisplay();
         setDisplay2d(0, 0, 64, 32);
         display_image(0, display);
       }
     
-      if(btns & 0x3){
+      if(btns & 0x2){
         clearDisplay();
         setDisplay2d(64, 0, 64, 32);
         display_image(0, display);
@@ -51,9 +59,9 @@ void user_isr( void )
 /* Lab-specific initialization goes here */
 void labinit( void )
 {
-  TRISE = TRISE & 0xffffff00;
   TRISD = TRISD & 0xf01f;
   TRISD += 0xfe0;
+  TRISF = TRISF | (0x20);
 
   T2CONSET = 0 << 15; // turn timer on
   TMR2 = 0; // set timer to 0
@@ -62,7 +70,6 @@ void labinit( void )
   IFSCLR(0) = 1 << 8; // set timer interupt flag to 0
   T2CONSET = 1 << 15; // start the timer
   IECSET(0) = (1 << 8); // timer 2 interrupt enable to 1 
-  IECSET(0) = (1 << 15); // switch 3 interrupt enable to 1
   IPCSET(2) = 0x1f; // set timer2 interrupt priority and sub-priority control to 1 
 
   enable_interrupt();
